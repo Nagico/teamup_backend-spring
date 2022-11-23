ab2b828accf20476d0f2c1c4c1401b8ce5c48688
Nagico <yjr888@vip.qq.com>
2022-11-23 17:51:22 +0800

    feat(swagger): 添加jwt认证

--- teamup-web/src/main/kotlin/cn/net/ziqiang/teamup/backend/web/annotation/user/ActiveUser.kt
+++ teamup-web/src/main/kotlin/cn/net/ziqiang/teamup/backend/web/annotation/user/ActiveUser.kt
@@ -1,9 +1,12 @@
 package cn.net.ziqiang.teamup.backend.web.annotation.user
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+
 /**
  * 需要验证是否为激活用户
  */
 @MustBeDocumented
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.FUNCTION)
+@SecurityRequirement(name = "JWT Auth")
 annotation class ActiveUser
--- teamup-web/src/main/kotlin/cn/net/ziqiang/teamup/backend/web/config/SpringDocConfig.kt
+++ teamup-web/src/main/kotlin/cn/net/ziqiang/teamup/backend/web/config/SpringDocConfig.kt
@@ -1,20 +1,17 @@
 package cn.net.ziqiang.teamup.backend.web.config
 
-import cn.net.ziqiang.teamup.backend.common.constant.ResultType
-import cn.net.ziqiang.teamup.backend.service.vo.ResultVO
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
-import io.swagger.v3.oas.models.media.Content
-import io.swagger.v3.oas.models.media.Schema
-import io.swagger.v3.oas.models.responses.ApiResponse
-import org.springdoc.core.customizers.OpenApiCustomiser
+import io.swagger.v3.oas.models.info.License
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.MediaType
 
 
 @Configuration
 class SpringDocConfig {
+    private val securitySchemeName = "JWT Auth"
 
     @Bean
     fun openApi(): OpenAPI {
@@ -22,34 +19,25 @@ class SpringDocConfig {
             .info(
                 Info()
                     .title("赛道有你API文档")
+                    .license(
+                        License()
+                            .name("AGPL-3.0")
+                            .url("https://www.gnu.org/licenses/agpl-3.0.html")
+                    )
+            )
+//            .addSecurityItem(
+//                SecurityRequirement()
+//                    .addList(securitySchemeName)
+//            )
+            .components(
+                Components()
+                    .addSecuritySchemes(securitySchemeName, SecurityScheme()
+                        .name(securitySchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("Bearer")
+                        .bearerFormat("JWT")
+                    )
             )
     }
 
-    @Bean
-    fun apiCustomizer(): OpenApiCustomiser {
-        return OpenApiCustomiser {
-            it.paths.values.forEach { pathItem ->
-                pathItem.readOperations().forEach { operation ->
-                    val responses = operation.responses
-
-                    ResultType.values().forEach { resultCode ->
-                        val result = ResultVO<Any>(resultCode)
-                        val response = ApiResponse()
-                            .description("${resultCode.code}-${resultCode.name}")
-                            .content(
-                                Content()
-                                    .addMediaType(MediaType.APPLICATION_JSON_VALUE,
-                                        io.swagger.v3.oas.models.media.MediaType()
-                                            .schema(
-                                                Schema<ResultVO<Any>>()
-                                                    .example(result)
-                                            )
-                                    )
-                            )
-                        responses.addApiResponse(resultCode.code, response)
-                    }
-                }
-            }
-        }
-    }
 }
