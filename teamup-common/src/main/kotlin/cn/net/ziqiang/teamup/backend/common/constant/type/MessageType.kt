1c5fc6a032b57e372a3f4bfb5ba7e3de9edbca17
Nagico <yjr888@vip.qq.com>
2022-11-27 15:25:58 +0800

    feat(chat): 增加消息类型

--- teamup-common/src/main/kotlin/cn/net/ziqiang/teamup/backend/common/constant/type/MessageType.kt
+++ teamup-common/src/main/kotlin/cn/net/ziqiang/teamup/backend/common/constant/type/MessageType.kt
@@ -1,11 +1,11 @@
 package cn.net.ziqiang.teamup.backend.common.constant.type
 
 enum class MessageType {
-    Join,
-    Leave,
-
     System,
 
     Chat,
+    Ack,
 
+    Typing,
+    UnTyping,
 }
