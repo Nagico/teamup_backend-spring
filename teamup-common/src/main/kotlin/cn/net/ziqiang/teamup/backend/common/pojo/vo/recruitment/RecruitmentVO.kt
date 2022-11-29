d5377aed6c117182b341cf5d025a05f4a11caa53
Nagico <yjr888@vip.qq.com>
2022-11-29 14:33:02 +0800

    fix(recruitment): 修复招募详情返回角色字段内容

--- teamup-common/src/main/kotlin/cn/net/ziqiang/teamup/backend/common/pojo/vo/recruitment/RecruitmentVO.kt
+++ teamup-common/src/main/kotlin/cn/net/ziqiang/teamup/backend/common/pojo/vo/recruitment/RecruitmentVO.kt
@@ -2,18 +2,19 @@ package cn.net.ziqiang.teamup.backend.common.pojo.vo.recruitment
 
 import cn.net.ziqiang.teamup.backend.common.pojo.entity.Recruitment
 import cn.net.ziqiang.teamup.backend.common.pojo.entity.Role
+import cn.net.ziqiang.teamup.backend.common.pojo.vo.role.RoleVO
 import cn.net.ziqiang.teamup.backend.common.pojo.vo.team.TeamInfoVO
 
 data class RecruitmentVO(
     var id: Long,
     var team: TeamInfoVO,
-    var role: Role,
+    var role: RoleVO,
     var requirements: List<String>,
 ){
     constructor(recruitment: Recruitment) : this(
         recruitment.id!!,
         TeamInfoVO(recruitment.team!!),
-        recruitment.role!!,
+        RoleVO(recruitment.role!!),
         recruitment.requirements
     )
 }
