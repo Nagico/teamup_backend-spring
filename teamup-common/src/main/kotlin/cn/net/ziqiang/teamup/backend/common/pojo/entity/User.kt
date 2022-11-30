91135d83552a60db79abfe5fdeec00771a889356
Nagico <yjr888@vip.qq.com>
2022-11-30 19:27:42 +0800

    fix(user): 修复用户密码失效的问题

--- teamup-common/src/main/kotlin/cn/net/ziqiang/teamup/backend/common/pojo/entity/User.kt
+++ teamup-common/src/main/kotlin/cn/net/ziqiang/teamup/backend/common/pojo/entity/User.kt
@@ -11,7 +11,6 @@ import java.util.*
 import javax.persistence.*
 
 @Entity(name = "user")
-@JsonIgnoreProperties(value= ["handler"])
 class User(
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -28,7 +27,6 @@ class User(
 
     @Column(name = "password", nullable = false)
     @Schema(description = "密码")
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     var password: String = "",
 
     @Column(name = "student_id")
